5c1da93e844f4d16c135af0371e62a3b9e76caf6
25112cn402-khushi <25112cn402@glbitm.ac.in>
2026-06-26 15:29:00 +0530

    Create RemovingDuplicates.cpp

---
+++ DAY-24/RemovingDuplicates.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+   //initialising frequency array for each character of string
+    vector <int> frequency(26,0);
+    string str;
+    string result;
+
+//input for string
+    cout<<"ENTER STRING:";
+    cin>>str;
+
+//checking frequency of each character in string
+    for(char c:str){
+        frequency[c-'a']++;
+    }
+//loop for array of frequency
+    for(int i=0;i<26;i++){
+
+     //if frequency is more than 1 then we are concatenating the chaarcater in result string
+        if(frequency[i]>=1){
+
+            result+=i+97;
+            
+        }
+    }
+//printing resultant string
+    cout<<result<<" is compressed string";
+    return 0;
+}
